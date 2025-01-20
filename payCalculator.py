47bd225105927c0ae90c7bb75e33b17c87cefc78
Brett Kenney <Brett.Kenney@vu.com>
2025-01-20 13:57:54 -0600

    added int/float to input lines

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,10 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = int(input("Enter Hours:"))
+    rate = float(input("Enter rate:"))
+    print(hrs * rate)
+    
 
     # end assignment
 
@@ -13,3 +16,5 @@ def calculatePay():
 #Ignore this for now. 
 if __name__ == "__main__":
     calculatePay()
+
+
